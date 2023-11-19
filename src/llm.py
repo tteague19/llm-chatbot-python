18f651918849790d328a5e3205f828e8c6c14078
Thomas Teague <24764359+tteague19@users.noreply.github.com>
2023-11-19 10:31:44 -0500

    Populate the llm.py file
    This change adds two helper functions to the llm.py module that,
    respectively, create an embedding model and chat model. The current
    implementation is restricted to the use of OpenAI models, but we plan to
     extend functionality to other providers.

--- src/llm.py
+++ src/llm.py
@@ -1,1 +1,36 @@
 """Implements an LLM for use in the Streamlit application."""
+from langchain.chat_models import ChatOpenAI
+from langchain.embeddings import OpenAIEmbeddings
+
+
+def create_embedding_model(
+        api_key: str, model_name: str = "text-embedding-ada-002",
+) -> OpenAIEmbeddings:
+    """
+    Create an embedding model.
+
+    :param api_key: An API key to enable the use of a proprietary model
+    :type api_key: str
+    :param model_name: The name of the embedding model to use, defaults to
+        "text-embedding-ada-002"
+    :return: An embedding model
+    :rtype: OpenAIEmbeddings
+    """
+    return OpenAIEmbeddings(openai_api_key=api_key, model=model_name)
+
+
+def create_chat_llm(
+        api_key: str, model_name: str = "gpt-3.5-turbo",
+) -> ChatOpenAI:
+    """
+    Create a large language model for use in a chat-based context.
+
+    :param api_key: An API key to enable the use of a proprietary model
+    :type api_key: str
+    :param model_name: The name of the embedding model to use, defaults to
+        "gpt-3.5-turbo"
+    :type model_name: str
+    :return: A chat-based large language model
+    :rtype: ChatOpenAI
+    """
+    return ChatOpenAI(model_name=model_name, openai_api_key=api_key)
